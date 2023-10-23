30d61bc6e1944caae55e3feedcb576db008d0131
jamsheerply <jamsheerpayyoli@gmail.com>
2023-10-23 11:34:55 +0530

    gitignore added

---
+++ middleware/userAuth.js
@@ -0,0 +1,28 @@
+
+
+const isLogin = async (req, res, next) => {
+    try {
+
+        if (!req.session.user_id) {
+            return res.redirect("/")
+        }
+        next();
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const isLogout = async (req, res, next) => {
+    try {
+        if (req.session.user_id && req.session.is_admin == 0) {
+            return res.redirect("/home")
+        }
+        next()
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+module.exports = {
+    isLogin,
+    isLogout
+}
